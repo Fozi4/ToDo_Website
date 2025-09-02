68d739086bfef5573f9f9a9144dc2333c964de6f
Fozi4 <bajmaran@gmail.com>
2025-09-02 16:58:50 +0200

    Datavase for Task created, basic controllers added

---
+++ ToDo_backend/Controllers/TaskItemController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ToDo_backend.Models;
+
+namespace ToDo_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskItemController : ControllerBase
+    {
+        private readonly TaskItemContext _context;
+
+        public TaskItemController(TaskItemContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TaskItem
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TaskItem>>> GetTaskItems()
+        {
+            return await _context.TaskItems.ToListAsync();
+        }
+
+        // GET: api/TaskItem/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TaskItem>> GetTaskItem(int id)
+        {
+            var taskItem = await _context.TaskItems.FindAsync(id);
+
+            if (taskItem == null)
+            {
+                return NotFound();
+            }
+
+            return taskItem;
+        }
+
+        // PUT: api/TaskItem/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTaskItem(int id, TaskItem taskItem)
+        {
+            if (id != taskItem.TaskId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(taskItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TaskItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/TaskItem
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<TaskItem>> PostTaskItem(TaskItem taskItem)
+        {
+            _context.TaskItems.Add(taskItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTaskItem", new { id = taskItem.TaskId }, taskItem);
+        }
+
+        // DELETE: api/TaskItem/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTaskItem(int id)
+        {
+            var taskItem = await _context.TaskItems.FindAsync(id);
+            if (taskItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.TaskItems.Remove(taskItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TaskItemExists(int id)
+        {
+            return _context.TaskItems.Any(e => e.TaskId == id);
+        }
+    }
+}
---
+++ ToDo_backend/Models/TaskItem.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDo_backend.Models
+{
+    public class TaskItem
+    {
+        [Key]
+        public int TaskId { get; set; }
+        public string TaskName { get; set; } = string.Empty;
+        public bool IsDone { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
