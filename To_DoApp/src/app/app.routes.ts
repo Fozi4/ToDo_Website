6756bb134cae994d64f696d50f47ef5df8641577
Fozi4 <bajmaran@gmail.com>
2025-09-03 21:13:09 +0200

    minor changes to home, app, and shared pages. Login page added. Add navigation from login to registration page.

--- To_DoApp/src/app/app.routes.ts
+++ To_DoApp/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home';
 import { TaskItemsComponent } from './task-items/task-items';
-
+import { LoginComponent } from './login/login'
+import { RegisterComponent } from './register/register'
 export const routes: Routes = [
-  { path: '', component: HomeComponent },       // головна сторінка
-  { path: 'tasks', component: TaskItemsComponent } // сторінка тасків
+  { path: '', component: HomeComponent },       // Main Page
+  { path: 'tasks', component: TaskItemsComponent }, // Tasks Page
+  { path: 'login', component: LoginComponent }, // Login Page
+  { path: 'register', component: RegisterComponent } // Register Page
 ];
--- To_DoApp/src/app/home/home.ts
+++ To_DoApp/src/app/home/home.ts
@@ -13,4 +13,5 @@ export class HomeComponent  {
   goToTasks(){
     this.router.navigate(['/tasks'])
   }
+  
 }
